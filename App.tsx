ac979c10abe52072fd8877b7220ae216749c7cfb
c1c3ru <cicerosilva.ifce@gmail.com>
2025-09-24 16:27:11 -0300

    Refatora o componente App para utilizar ThemeProvider e AuthProvider, removendo a estrutura anterior e integrando a navegação com AppNavigator.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from './src/presentation/contexts/ThemeContext';
+import { AuthProvider } from './src/presentation/contexts/AuthProvider';
+import AppNavigator from './src/presentation/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      <AuthProvider>
+        <StatusBar style="auto" />
+        <AppNavigator />
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
