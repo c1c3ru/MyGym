249982bf15c1b89a6f661dab09ff7276a1232e64
c1c3ru <cicerosilva.ifce@gmail.com>
2025-12-30 15:17:28 -0300

    refactor: Remover imports não utilizados de `ThemeToggleContext` e `getString`.

--- src/presentation/screens/auth/RegisterScreen.js
+++ src/presentation/screens/auth/RegisterScreen.js
@@ -19,8 +19,6 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useAuthFacade } from '@presentation/auth/AuthFacade';
 import { useTheme } from '@contexts/ThemeContext';
 import { COLORS, SPACING, FONT_SIZE, BORDER_RADIUS, FONT_WEIGHT } from '@presentation/theme/designTokens';
-import { useThemeToggle } from '@contexts/ThemeToggleContext';
-import { getString } from '@utils/theme';
 
 const { width } = Dimensions.get('window');
 
